2b7e792fb55e5139e3a34132279061f5150a0024
Arghyadeep Mallik <argz.social2008@gmail.com>
2026-08-27 10:30:24 +0530

    Q136-SingleNumber solution using XOR approach
    Implement XOR solution to find single number in an array.

---
+++ q136.cpp
@@ -0,0 +1,27 @@
+//Single Number
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    //best method(XOR solution)
+         int sum=0;
+        for(int i:nums){
+            sum=sum^i;
+        }  
+        return sum;
+    
+    //beginner's method
+    /*    int count=0,a;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+                a=nums[i];
+                break;}
+            count=0;
+        }
+        return a; */
+    }
+};
