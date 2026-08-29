4cadb44e9adc2baa99acecfd5d588d9d75ed7bf5
argz-code <arghyadeep.exam@gmail.com>
2026-08-29 12:34:10 +0530

    ques no.53

--- max_subarray_sum.cpp
+++ max_subarray_sum.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<limits.h>
+#include<vector>
 using namespace std;
 
 int main(){
+    //brute force 
+    /*
     int arr[]={3,-1,7,-4,5,9,-3,-7,10};
     int maxsum=INT_MIN;
     int cursum=0;
@@ -11,6 +14,17 @@ int main(){
         for(int j=i;j<9;j++){
             cursum=cursum+arr[j];
             maxsum=max(cursum,maxsum);
+        }
+    }
+    */
+   //kadane's algorithm
+    vector<int>nums={3,-1,7,-4,5,9,-3,-7,10};
+    int cursum=0,maxsum=INT_MIN;
+    for(int i:nums){
+        cursum+=i;
+        maxsum=max(cursum,maxsum);
+        if(cursum<0){
+            cursum=0;
         }
     }
     cout<<maxsum<<endl;
